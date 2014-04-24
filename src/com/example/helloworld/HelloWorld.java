14fef8e59b54b3de4e9a32ffca0f5b6f64b6f92c
Andrew HoChoy <a.hochoy@gmail.com>
2014-04-23 21:44:30 -0400

    Sample project, checking in

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by ahochoy on 4/23/14.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
